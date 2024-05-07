ba2bfe0cea18950f682aeba5a0512775ecdaa535
liujiang <jiang.liu5@pactera.com>
2024-05-07 17:22:13 +0800

    取消Java8兼容

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,10 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 dependencies {
