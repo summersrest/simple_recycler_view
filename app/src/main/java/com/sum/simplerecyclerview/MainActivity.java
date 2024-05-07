10584c7a7a7e4bd6a1c77fde56aef1bece66e140
liujiang <jiang.liu5@pactera.com>
2024-05-07 16:14:03 +0800

    版本更新

--- app/src/main/java/com/sum/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/sum/simplerecyclerview/MainActivity.java
@@ -1,14 +1,49 @@
 package com.sum.simplerecyclerview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewbinding.ViewBinding;
+
+import com.sum.simple.SimpleRecyclerView;
+import com.sum.simpleadapter.BaseAdapter;
+import com.sum.simpleadapter.base.ViewHolder;
+import com.sum.simplerecyclerview.databinding.ActivityMainBinding;
+import com.sum.simplerecyclerview.databinding.ItemMainBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author LiuJiang
+ * created  at: 2024/5/7 15:35
+ * Desc:
+ */
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding viewBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
+        List<String> data = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+        viewBinding.recyclerView.setAdapter(new BaseAdapter<ItemMainBinding, String>(this, data) {
+
+            @Override
+            protected ItemMainBinding getViewBinding(int viewType, LayoutInflater layoutInflater, ViewGroup parent) {
+                return ItemMainBinding.inflate(layoutInflater, parent, false);
+            }
+
+            @Override
+            protected void onBind(Context context, ViewHolder<ItemMainBinding> holder, String item, int position) {
+                holder.binding.tvItemName.setText(item);
+            }
+        });
     }
 }
